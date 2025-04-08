c408e0fb70d0d5a748f7d716e2436d6eb089e9d1
Tony <tonyhx2021@gmail.com>
2025-04-08 15:44:15 -0400

    format code

--- src/app/social/social.component.ts
+++ src/app/social/social.component.ts
@@ -1,17 +1,22 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit} from '@angular/core';
+import {
+	ChangeDetectionStrategy,
+	ChangeDetectorRef,
+	Component,
+	Input,
+	OnInit,
+} from '@angular/core';
 
-declare var data : any;
+declare var data: any;
 
 @Component({
 	selector: 'app-social',
 	templateUrl: './social.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
-	styleUrls: ['./social.component.css']
+	styleUrls: ['./social.component.css'],
 })
-
 export class SocialComponent implements OnInit {
 	public socialData = data['Social'];
-	@Input() color : String = "black";
+	@Input() color: String = 'black';
 
 	constructor(private changeDetectorRef: ChangeDetectorRef) {
 		changeDetectorRef.detach();
@@ -20,5 +25,4 @@ export class SocialComponent implements OnInit {
 	ngOnInit(): void {
 		this.changeDetectorRef.detectChanges();
 	}
-
 }
