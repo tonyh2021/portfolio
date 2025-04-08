a997582e68d71f8873875549a7971c580c420364
Tony <tonyhx2021@gmail.com>
2025-04-08 13:56:01 -0400

    format code.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,29 +1,34 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, HostListener, OnInit} from '@angular/core';
+import {
+	ChangeDetectionStrategy,
+	ChangeDetectorRef,
+	Component,
+	HostListener,
+	OnInit,
+} from '@angular/core';
 
-declare var data : any;
-declare var identity : any;
+declare var data: any;
+declare var identity: any;
 
 @Component({
 	selector: 'app-navbar',
 	templateUrl: './navbar.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
-	styleUrls: ['./navbar.component.css']
+	styleUrls: ['./navbar.component.css'],
 })
-
 export class NavbarComponent implements OnInit {
 	public navbarData = data['NavBar'];
 	public identityData = identity;
-	public activeClass: String = "Home";
+	public activeClass: String = 'Home';
 
-	private offset :any = [];
-	private offsetLink : any = [];
+	private offset: any = [];
+	private offsetLink: any = [];
 
-	private size : number = 0;
-	private firstScroll : boolean = true;
-	private firstClick : boolean = true;
-	public navbarProfileVisibility : boolean = false;
-	public sticky : boolean = false;
-	public animation : string = "";
+	private size: number = 0;
+	private firstScroll: boolean = true;
+	private firstClick: boolean = true;
+	public navbarProfileVisibility: boolean = false;
+	public sticky: boolean = false;
+	public animation: string = '';
 
 	constructor(private changeDetectorRef: ChangeDetectorRef) {}
 
@@ -33,35 +38,36 @@ export class NavbarComponent implements OnInit {
 			this.offset.push(0);
 			this.size++;
 		}
-		window.addEventListener('keydown',(event)=>{
-			if (event.code === "Escape" && this.navbarProfileVisibility){
+		window.addEventListener('keydown', (event) => {
+			if (event.code === 'Escape' && this.navbarProfileVisibility) {
 				this.removeProfile();
 			}
-		})
+		});
 	}
-	ngAfterViewInit(){
+	ngAfterViewInit() {
 		let navTabs: any = document.querySelectorAll('.nav-link a');
 		for (const navTab of navTabs) {
-			navTab.addEventListener('click',(event:any) => event.preventDefault());
+			navTab.addEventListener('click', (event: any) =>
+				event.preventDefault()
+			);
 		}
 	}
 
-	private binarySearch(target : number) : number{
+	private binarySearch(target: number): number {
 		let low = 0;
 		let high = this.offset.length - 1;
 
-		if(target <= this.offset[low]){
+		if (target <= this.offset[low]) {
 			return 0;
 		}
-		if(target >= this.offset[high]){
+		if (target >= this.offset[high]) {
 			return high;
 		}
 		let res = 0;
-		while(low<high)
-		{
-			let mid = Math.floor((low + high)/2);
+		while (low < high) {
+			let mid = Math.floor((low + high) / 2);
 
-			if(target < this.offset[mid]) {
+			if (target < this.offset[mid]) {
 				high = mid;
 			} else {
 				res = mid;
@@ -71,8 +77,8 @@ export class NavbarComponent implements OnInit {
 		return res;
 	}
 
-	@HostListener('window:scroll',['$event'])
-	onWindowScroll(){
+	@HostListener('window:scroll', ['$event'])
+	onWindowScroll() {
 		if (this.firstScroll) {
 			this.updateOffsetLink();
 			this.firstScroll = false;
@@ -84,7 +90,7 @@ export class NavbarComponent implements OnInit {
 	}
 
 	@HostListener('window:resize', ['$event'])
-	onResize(){
+	onResize() {
 		this.firstClick = true;
 		this.firstScroll = true;
 		if (window.innerWidth >= 992 && this.navbarProfileVisibility) {
@@ -92,52 +98,62 @@ export class NavbarComponent implements OnInit {
 		}
 	}
 
-	public scrollTo (target : any, duration = 1500, element : any = document.scrollingElement) {
+	public scrollTo(
+		target: any,
+		duration = 1500,
+		element: any = document.scrollingElement
+	) {
 		if (element.scrollTop === target) return;
 
 		const cosParameter = (element.scrollTop - target) / 2;
 		let scrollCount = 0;
-		let oldTimestamp : any = null;
+		let oldTimestamp: any = null;
 
-		const step = (newTimestamp : any)=>{
+		const step = (newTimestamp: any) => {
 			if (oldTimestamp !== null) {
-				scrollCount += Math.PI * (newTimestamp - oldTimestamp) / duration;
+				scrollCount +=
+					(Math.PI * (newTimestamp - oldTimestamp)) / duration;
 				if (scrollCount >= Math.PI) {
 					element.scrollTop = target;
 					return target;
-				}else{
-					element.scrollTop = cosParameter + target + cosParameter * Math.cos(scrollCount);
+				} else {
+					element.scrollTop =
+						cosParameter +
+						target +
+						cosParameter * Math.cos(scrollCount);
 				}
 			}
 			oldTimestamp = newTimestamp;
 			window.requestAnimationFrame(step);
-		}
+		};
 		window.requestAnimationFrame(step);
 	}
 
-	updateActiveLink(navLink : String) {
-		if(this.firstClick){
+	updateActiveLink(navLink: String) {
+		if (this.firstClick) {
 			this.updateOffsetLink();
 			this.firstClick = false;
 		}
 		this.activeClass = navLink;
 		let t = 0;
-		if(this.navbarProfileVisibility) {
+		if (this.navbarProfileVisibility) {
 			this.removeProfile();
 			t = 900;
 		}
-		setTimeout(()=>{
+		setTimeout(() => {
 			let element = document.getElementById(navLink.toLowerCase());
-			if(element) {
+			if (element) {
 				this.scrollTo(element.offsetTop);
 			}
-		},t);
+		}, t);
 	}
 
-	public updateOffsetLink(){
-		for (let index = 0;index < this.size;index++) {
-			let element = document.getElementById(this.navbarData['links'][index].toLowerCase());
-			if(element) {
+	public updateOffsetLink() {
+		for (let index = 0; index < this.size; index++) {
+			let element = document.getElementById(
+				this.navbarData['links'][index].toLowerCase()
+			);
+			if (element) {
 				this.offset[index] = element.offsetTop;
 			}
 		}
@@ -146,14 +162,14 @@ export class NavbarComponent implements OnInit {
 	public removeProfile() {
 		this.animation = 'slideOutRight 1s forwards';
 		this.changeDetectorRef.detectChanges();
-		
-		setTimeout(()=>{
+
+		setTimeout(() => {
 			this.navbarProfileVisibility = false;
-			if(this.animation === 'slideOutRight 1s forwards'){
+			if (this.animation === 'slideOutRight 1s forwards') {
 				this.animation = '';
 				this.changeDetectorRef.detectChanges();
 			}
-		},1000);
+		}, 1000);
 	}
 
 	public addProfile() {
