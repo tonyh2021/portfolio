e2ad111fe896df8f0ca2a158679a086a7c205f1c
Tony <tonyhx2021@gmail.com>
2025-04-09 16:00:43 -0400

    update footer.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,14 +1,18 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {
+	ChangeDetectionStrategy,
+	ChangeDetectorRef,
+	Component,
+	OnInit,
+} from '@angular/core';
 
-declare var data : any
+declare var data: any;
 
 @Component({
 	selector: 'app-footer',
 	templateUrl: './footer.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
-	styleUrls: ['./footer.component.css']
+	styleUrls: ['./footer.component.css'],
 })
-
 export class FooterComponent implements OnInit {
 	public footerData = data['Footer'];
 
@@ -19,5 +23,4 @@ export class FooterComponent implements OnInit {
 	ngOnInit(): void {
 		this.changeDetectorRef.detectChanges();
 	}
-
 }
